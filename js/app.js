5cd6160b8c97f64bf867eab03a6b8d851e0a9505
ax8tje <131400863+ax8tje@users.noreply.github.com>
2024-12-10 17:42:16 +0100

    tetris_app_1.1

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const lTetromino = 'firstShape'
+const zTetromino = 'secondShape'
+const oTetromino = 'thirdShape'
+const iTetromino = 'fourthShape'
+const tTetromino = 'fifthShape'
+
+const tetrominoes = [lTetromino,zTetromino,oTetromino,iTetromino,tTetromino]
+
+console.log(tetrominoes[0])
