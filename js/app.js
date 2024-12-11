997e6854ed2c37d063ea456735e81dfd8e01b96a
ax8tje <131400863+ax8tje@users.noreply.github.com>
2024-12-11 22:52:21 +0100

    tetris_app_1.3
    minor fixes + j and s tetrominoes

--- js/app.js
+++ js/app.js
@@ -7,24 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const lTetromino = [
-        [1,width+1, width*2+1,2],
-        [width, width+1, width+2,width*2+2],
-        [1, width+1, width*2+1,width*2],
+        [1, width+1, width*2+1 ,2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
         [width, width*2, width*2+1, width*2+2]
     ]
 
     const zTetromino = [
-        [0,width,width+1,width*2+1],
-        [width + 1, width+2,width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width + 1, width+2, width*2, width*2+1],
         [0,width,width+1,width*2+1],
         [width + 1, width+2,width*2, width*2+1]
     ]
 
     const tTetromino = [
-        [1,width,width+1,width+2],
-        [1,width+1,width+2,width+2*1],
-        [width,width+1,width+2,width+2*1],
-        [1,width,width+1,width*2+1]
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width + 1, width * 2 + 1, width + 2]
     ]
 
     const oTetromino = [
@@ -35,13 +35,28 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const iTetromino = [
-        [1,width+1,width*2+1,width*3+1],
-        [width,width+1,width+2,width+3],
-        [1,width+1,width*2+1,width*3+1],
-        [width,width+1,width+2,width+3]
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
     ]
 
-    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+    const jTetromino = [
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width + 1, width + 2, width* 2 + 2],
+        [1, width + 1, width * 2 + 1, 2],
+        [0, width, width+1, width+2]
+    ]
+    
+
+    const sTetromino = [
+        [1, width + 1, width, width * 2],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [1, width + 1, width, width * 2],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+
+    const theTetrominoes = [jTetromino, lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4
     let currentRotation = 0
